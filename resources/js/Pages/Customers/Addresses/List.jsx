eb686a6d647c9c559718f8533b3ee0cb808ca573
Behzad Fazelasl <behzad.fazelasl@gmail.com>
2023-08-20 10:23:52 +0200

    add button to open the create new address modal

--- resources/js/Pages/Customers/Addresses/List.jsx
+++ resources/js/Pages/Customers/Addresses/List.jsx
@@ -2,6 +2,8 @@ import React, {useEffect, useState} from 'react';
 import DashboardLayout from '../../../Layouts/DashboardLayout.jsx';
 import axios from "axios";
 import {store} from "@/store/configureStore.js";
+import { InertiaLink } from '@inertiajs/inertia-react';
+import AddAddressModal from './AddAddressModal';
 
 const ShowList = ({uuid}) => {
     console.log(uuid);
@@ -20,9 +22,28 @@ const ShowList = ({uuid}) => {
             });
     }, []);
 
+    const [isModalOpen, setIsModalOpen] = useState(false);
+
+    const openModal = () => {
+        setIsModalOpen(true);
+    };
+
+    const closeModal = () => {
+        setIsModalOpen(false);
+    };
+
+
     return (
         <DashboardLayout>
-            <h2 className="text-xl font-semibold mb-4">Show Addresses</h2>
+            <div className="flex justify-between items-center mb-4">
+                <h2 className="text-xl font-semibold">Show Addresses</h2>
+                <button
+                    onClick={openModal}
+                    className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600 focus:outline-none focus:ring focus:ring-blue-200"
+                >
+                    Add New Address
+                </button>
+            </div>
             <div className="overflow-x-auto">
                 <table className="min-w-full border">
                     <thead>
@@ -47,6 +68,11 @@ const ShowList = ({uuid}) => {
                     </tbody>
                 </table>
             </div>
+            <AddAddressModal
+                isOpen={isModalOpen}
+                onClose={closeModal}
+                customerId={uuid}
+            />
         </DashboardLayout>
     );
 };
